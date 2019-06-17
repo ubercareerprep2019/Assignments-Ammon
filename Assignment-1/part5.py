a3fc700b9733d5f265529f91f807422d487d2c6e
Ammon Mugimu <amugimu@gmail.com>
2019-06-16 20:49:02 -0700

    Fixed function signatures.

--- Assignment-1/part5.py
+++ Assignment-1/part5.py
@@ -4,23 +4,38 @@ class TowerOfHanoi:
         self.second_rod = []
         self.third_rod = []
         self.n = n
+        self.number_of_rods = 3
 
     def move_disks(self):
+        self.move_disk_recursive(self.n, self.first_rod, self.second_rod, self.third_rod)
 
-        self.move_disk(self.n, self.first_rod, self.second_rod, self.third_rod)
-
-    def move_disk(self, n, first, second, third):
+    def move_disk_recursive(self, n, first, second, third):
         if n >= 1:
-            self.move_disk(n-1, first, third, second)
+            self.move_disk_recursive(n-1, first, third, second)
 
-            disk_from_first_rod = first.pop()
-            third.append(disk_from_first_rod)
+            self.move_disk(first, third)
 
-            self.move_disk(n-1, second, first, third)
+            self.move_disk_recursive(n-1, second, first, third)
+
+    def move_disk(self, starting_rod, destination_rod):
+        destination_rod.append(starting_rod.pop())
+
+    def number_of_rods(self):
+        return self.number_of_rods
+
+    def disks_at_rod(self, index):
+        if index >= 0 and index < self.number_of_rods():
+            if index is 0:
+                return len(self.first_rod)
+            elif index is 1:
+                return len(self.second_rod)
+            elif index is 2:
+                return len(self.third_rod)
+        else:
+            return 0
 
 
-
-hanoi = TowerOfHanoi(9)
+hanoi = TowerOfHanoi(6)
 print("Before Move.")
 print(hanoi.first_rod)
 print(hanoi.second_rod)
